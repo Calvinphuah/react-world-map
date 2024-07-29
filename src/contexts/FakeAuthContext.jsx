d08470a51974350d8b9350bff2615db61da5c4a0
calvinphuah <calvinphuah73@gmail.com>
2024-07-29 23:16:23 +0800

    completed

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -52,6 +52,7 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside of the AuthProvider");
+  return context;
 }
 
 export { AuthProvider, useAuth };
